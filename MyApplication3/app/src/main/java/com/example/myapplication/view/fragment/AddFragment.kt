4d0e1400600a859a9f19296315736302fd2bb0a7
도현욱 <105199741+wsi1212@users.noreply.github.com>
2023-02-07 22:27:35 +0900

    ADD:postadd

---
+++ MyApplication3/app/src/main/java/com/example/myapplication/view/fragment/AddFragment.kt
@@ -0,0 +1,35 @@
+package com.example.myapplication.view.fragment
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.myapplication.viewmodel.AddViewModel
+import com.example.myapplication.R
+import com.example.myapplication.databinding.FragmentAddBinding
+
+class AddFragment : Fragment() {
+    private lateinit var binding: FragmentAddBinding
+    companion object {
+        fun newInstance() = AddFragment()
+    }
+
+    private lateinit var viewModel: AddViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_add, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(AddViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
