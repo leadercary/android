178b96a80f319e5744e8e8095eed49ceac777cc5
도현욱 <105199741+wsi1212@users.noreply.github.com>
2023-02-08 04:54:42 +0900

    Update : LoginService

--- MyApplication3/app/src/main/java/com/example/myapplication/network/service/LoginService.kt
+++ MyApplication3/app/src/main/java/com/example/myapplication/network/service/LoginService.kt
@@ -3,11 +3,13 @@ package com.example.myapplication.network.service
 import com.example.myapplication.network.model.LoginDto
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.Header
 import retrofit2.http.POST
 
 interface LoginService {
     @POST("/api/user/signin")
     fun signIn(
+        @Header("Authorization") token: String?,
         @Body login:LoginDto
     ): Call<String>
 }
