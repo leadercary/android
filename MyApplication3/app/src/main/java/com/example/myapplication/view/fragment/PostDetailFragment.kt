b78d30d1297704f5a1c992d22a75ffcc40611079
도현욱 <105199741+wsi1212@users.noreply.github.com>
2023-02-08 02:08:21 +0900

    FIX: Nav

--- MyApplication3/app/src/main/java/com/example/myapplication/view/fragment/PostDetailFragment.kt
+++ MyApplication3/app/src/main/java/com/example/myapplication/view/fragment/PostDetailFragment.kt
@@ -1,6 +1,5 @@
 package com.example.myapplication.view.fragment
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -21,7 +20,7 @@ class PostDetailFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_fragment_post_detail, container, false)
+        return inflater.inflate(R.layout.fragment_post_detail, container, false)
     }
 
 }
